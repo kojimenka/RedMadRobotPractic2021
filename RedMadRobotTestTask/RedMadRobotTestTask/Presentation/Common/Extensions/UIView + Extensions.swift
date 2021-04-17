0c177c59062397ea2a179098d1da103cd1f07e66
Dmitrii Marchenkov (intern) <kojimenka@gmail.com>
2021-04-17 20:21:34 +0300

    Set initial files in right struct, add localization

---
+++ RedMadRobotTestTask/RedMadRobotTestTask/Presentation/Common/Extensions/UIView + Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  UIView + Extensions.swift
+//  Highlights
+//
+//  Created by Дмитрий Марченков on 07.04.2021.
+//
+
+import UIKit
+
+extension UIView {
+    func animateShow() {
+        UIView.animate(withDuration: 0.3) {
+            self.alpha = 1.0
+        }
+    }
+    
+    func animateHide() {
+        UIView.animate(withDuration: 0.3) {
+            self.alpha = 0.0
+        }
+    }
+}
+
+//MARK: - Shadows
+extension UIView {
+    func applyShadow(shadowOffSet: CGSize, shadowOpacity: Float, shadowRadius: CGFloat, color: UIColor) {
+        layer.shadowColor = color.cgColor
+        layer.masksToBounds = false
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: layer.cornerRadius).cgPath
+        layer.shadowOffset = shadowOffSet
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+    }
+}
+
