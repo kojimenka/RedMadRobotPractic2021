2ed2694eb48fcbd499fd40dd3908835db20e160d
Dmitrii Marchenkov (intern) <kojimenka@gmail.com>
2021-04-17 22:23:12 +0300

    Add resources: colors, fonts, images

--- RedMadRobotTestTask/RedMadRobotTestTask/Presentation/Common/Style/ColorPalette.swift
+++ RedMadRobotTestTask/RedMadRobotTestTask/Presentation/Common/Style/ColorPalette.swift
@@ -5,4 +5,16 @@
 //  Created by Дмитрий Марченков on 17.04.2021.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    struct ColorPalette {
+        
+        // MARK: - General Colors
+        static var tintOrangeColor = R.color.tintOrange()
+        static var grayTitleColorText = R.color.dark_grey()
+        static var subtitleTextColor = R.color.middle_grey()
+        static var templatesTintColor = R.color.grey()
+        static var notActive = R.color.light_grey_blue()
+    }
+}
